0794ac07246e36b3d3155a4795bafe15d3b38441
IlyaChistov <gaplis322@gmail.com>
2022-09-08 17:11:49 +0300

    Lesson 4

---
+++ example4_015_Factorial/Program.cs
@@ -0,0 +1,15 @@
+﻿double Factorial (int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if(n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+
+Console.WriteLine(Factorial(3)); // 1 * 2 * 3 = 6
+Console.WriteLine(Factorial(5));
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
